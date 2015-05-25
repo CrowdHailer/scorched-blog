867644c4455a0b9dd7a473b5683f39eec49683c3
Peter Saxton <peterhsaxton@gmail.com>
2015-05-25 08:05:50 +0100

    overkill on the requirements

--- lib/post.rb
+++ lib/post.rb
@@ -1,2 +1,10 @@
-require_relative './post/record'
-require_relative './post/entity'
+module Post
+  def self.new(*args, &block)
+    Entity.new(*args, &block)
+  end
+end
+Dir[File.join(
+  File.dirname(__FILE__),
+  File.basename(__FILE__, '.*'),
+  '*.rb')
+].each {|file| require file }
